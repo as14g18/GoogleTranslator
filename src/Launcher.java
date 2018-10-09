96cf519e95276e167b21eb49dd8182247550e795
as14g18 <as14g18@soton.ac.uk>
2018-10-09 15:42:40 +0100

    Initial commit

---
+++ src/Launcher.java
@@ -0,0 +1,8 @@
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		Translator EtoF = new Translator("English", "French");
+	}
+
+}
---
+++ src/Translator.java
@@ -0,0 +1,14 @@
+
+public class Translator {
+	private String langIn;
+	private String langOut;
+	
+	public Translator(String langIn, String langOut) {
+		this.langIn = langIn;
+		this.langOut = langOut;
+	}
+	
+	public String translate(String word) {
+		return "";
+	}
+}
